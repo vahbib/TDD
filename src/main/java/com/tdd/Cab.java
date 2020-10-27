5118a6c5c5514ee240799f6ce98ff64306ea4a75
Bibhav Singh <singhbibhav084@gmail.com>
2020-10-27 23:18:04 +0530

    Given distance and time, the invoice generator should return the total fare for the journey

--- src/main/java/com/tdd/Cab.java
+++ src/main/java/com/tdd/Cab.java
@@ -1,5 +1,20 @@
 package com.tdd;
 
+
 public class Cab {
 
+
+    public int calculateFare(int k, int m) {
+
+        int f = (k*10) + (m);
+        int n = f;
+        if (n <=5) {
+            f = 5;
+            System.out.println("Total Fare is 5 Rupees.");
+        }else {
+            System.out.println("Total Fare is " + f + " Rupees.");
+        }
+        return f;
+
+    }
 }
--- src/test/java/com/tdd/TddDemo.java
+++ src/test/java/com/tdd/TddDemo.java
@@ -4,6 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TddDemo {
+    @Test
+    public void shouldReturnTotalFareOfJourney() {
+        Cab cab = new Cab();
+        int total = cab.calculateFare(0, 3);
+        Assert.assertEquals(5, total);
+
+    }
 
 
 }
