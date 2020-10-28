67146832e90b45b215e78418811d8318c24e2e71
Bibhav Singh <singhbibhav084@gmail.com>
2020-10-28 12:15:00 +0530

    The Invoice Generator should now take in multiple rides and calculate the aggregate total for all

--- src/main/java/com/tdd/Cab.java
+++ src/main/java/com/tdd/Cab.java
@@ -1,5 +1,18 @@
 package com.tdd;
 
 public class Cab {
+    int total = 0;
 
+    public int calculateFare(int km, int time) {
+
+        int t = time + (10 * km);
+        int n = t;
+        if (n <= 5) {
+            t = 5;
+            total = total + t;
+        } else {
+            total = total + t;
+        }
+        return total;
+    }
 }
--- src/test/java/com/tdd/TddDemo.java
+++ src/test/java/com/tdd/TddDemo.java
@@ -4,6 +4,17 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TddDemo {
+    @Test
+    public void shouldReturnTotalFareOfJourney() {
+        Cab cab = new Cab();
+        cab.calculateFare(0, 3);
+        cab.calculateFare(3, 4);
+        cab.calculateFare(4, 5);
+        cab.calculateFare(5, 6);
+
+        Assert.assertEquals(140, cab.total);
+
+    }
 
 
 }
