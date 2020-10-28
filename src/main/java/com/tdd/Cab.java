a94fe7e276abc016f0f26476165715d16a3ac5e7
Bibhav Singh <singhbibhav084@gmail.com>
2020-10-28 16:14:49 +0530

    PREMIUM & NORMAL rides

--- src/main/java/com/tdd/Cab.java
+++ src/main/java/com/tdd/Cab.java
@@ -1,5 +1,108 @@
 package com.tdd;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+class Invoice{
+    public int total;
+    public int noOfRides;
+    public String Id;
+    public double averageFare;
+
+    Invoice() {
+
+    }
+
+    Invoice(String Id,int total, int noOfRides, double averageFare)
+    {
+        this.Id=Id;
+        this.total=total;
+        this.noOfRides=noOfRides;
+        this.averageFare=averageFare;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "UserId : " + Id + "\nTotal Fare : " + total +
+                "\nTotal number of Rides: " + noOfRides + "\nAverage Amount : " + averageFare;
+    }
+}
+
 public class Cab {
+
+
+    int total = 0;
+    int noOfRides = 0;
+    public HashMap<String, ArrayList<Integer>> rides = new HashMap<>();
+    public ArrayList<Integer> fareDetails;
+
+    public enum type {
+        NORMAL,
+        PREMIUM
+    }
+
+    public void FareDetail(String Id, type rideType,  int km, int time) {
+        if (rideType == type.NORMAL) {
+
+            int f = calculateFareNormal(km, time);
+            System.out.println("\nFare: " + f);
+            noOfRides++;
+            ArrayList<Integer> rideDet = new ArrayList<>();
+            if (rides.containsKey(Id)) {
+                rideDet = rides.get(Id);
+            }
+            rideDet.add(f);
+            rides.put(Id, rideDet);
+            System.out.println(rides);
+        } else if (rideType == type.PREMIUM) {
+
+            int f = calculateFarePremium(km, time);
+            System.out.println("\nFare: " + f);
+            noOfRides++;
+            ArrayList<Integer> rideDet = new ArrayList<>();
+            if (rides.containsKey(Id)) {
+                rideDet = rides.get(Id);
+            }
+            rideDet.add(f);
+            rides.put(Id, rideDet);
+            System.out.println(rides);
+
+        }
+    }
+
+    public Invoice RideRepo(String Id) {
+        fareDetails = rides.get(Id);
+        int totalFare = 0;
+        for (int fare: fareDetails) {
+            totalFare = totalFare +fare;
+        }
+        int totalRides = fareDetails.size();
+        double avg = (double)totalFare/(double)totalRides;
+        return new Invoice(Id, totalFare,totalRides,avg);
+    }
+
+
+
+    public int calculateFareNormal(int km, int time) {
+        int t = time + (10 * km);
+        int n = t;
+        if (n <= 5) {
+            t = 5;
+        }
+        total = t;
+        return total;
+    }
+    public int calculateFarePremium(int km, int time) {
+        int t = (2*time) + (15 * km);
+        int n = t;
+        if (n <= 20) {
+            t = 20;
+        }
+        total = t;
+        return total;
+    }
+
 
 }
--- src/test/java/com/tdd/TddDemo.java
+++ src/test/java/com/tdd/TddDemo.java
@@ -4,6 +4,18 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TddDemo {
+    @Test
+    public void InvoiceServiceTest() {
+        Cab cab = new Cab();
+        Invoice invoice = new Invoice();
+        cab.FareDetail("Bibhav", Cab.type.NORMAL, 4, 2);
+        cab.FareDetail("Bibhav", Cab.type.PREMIUM ,7, 2);
+        cab.FareDetail("Bibhav", Cab.type.NORMAL,2, 8);
+
+        int total = cab.RideRepo("Bibhav").total;
+        System.out.println("Total Fare is "+total);
+        Assert.assertEquals(179, total);
+    }
 
 
 }
