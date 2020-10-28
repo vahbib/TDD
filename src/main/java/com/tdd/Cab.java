b73e81e0950c4936178707c21a35e9d9d87e83b5
Bibhav Singh <singhbibhav084@gmail.com>
2020-10-28 12:33:26 +0530

    Enhanced Invoice: The Invoice Generator should now return TOTAL NUMBER OF RIDES, TOTAL FARE, AVERAGE FARE PER RIDE

--- src/main/java/com/tdd/Cab.java
+++ src/main/java/com/tdd/Cab.java
@@ -2,4 +2,24 @@ package com.tdd;
 
 public class Cab {
 
+    int total = 0;
+    int noOfRides = 0;
+
+    public int calculateFare(int km, int time) {
+        noOfRides++;
+        int t = time + (10 * km);
+        int n = t;
+        if (n <= 5) {
+            t = 5;
+            total = total + t;
+        } else {
+            total = total + t;
+        }
+        return total;
+    }
+
+    public double AvgFare() {
+       double avgFare = (double) total / (double) noOfRides;
+       return avgFare;
+    }
 }
--- src/test/java/com/tdd/TddDemo.java
+++ src/test/java/com/tdd/TddDemo.java
@@ -4,6 +4,20 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TddDemo {
+    @Test
+    public void shouldReturn_TotalFare_AvgFare_NoOfRides() {
+        Cab cab = new Cab();
+        cab.calculateFare(4,5);
+        cab.calculateFare(5,2);
+        cab.calculateFare(8,0);
+        cab.calculateFare(0,4);
+
+        Assert.assertEquals(182, cab.total);
+        Assert.assertEquals(4, cab.noOfRides);
+        Assert.assertEquals(45.5, cab.AvgFare(), 0);
+
+
+    }
 
 
 }
